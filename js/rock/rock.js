37faa03c1393fc5656a678442073d271b33ae50e
Chris Fedosick <cfedosick@gmail.com>
2023-10-12 17:14:48 -0400

    updated messages within decision tree.

--- js/rock/rock.js
+++ js/rock/rock.js
@@ -11,7 +11,7 @@ function getComputerChoice()
 function getPlayerChoice()
 {
     player = prompt("Choose rock, paper, or scissors");
-    player.toLowerCase();
+    player = player.toLowerCase();
 }
 
 function compare()
@@ -21,51 +21,69 @@ function compare()
         case 0:
             if(player === 'rock')
             {
-                console.log('rock vs rock');
+                console.log('Tie! Rock vs. Rock');
             }
             else if (player === 'scissors')
             {
-               console.log('rock vs scissors');
+               console.log('Loser! Computer chose rock vs your scissor');
             }
             else if (player === 'paper')
             {
-                console.log('rock vs. paper');
+                console.log('Winner! You chose paper vs the computers rock!');
+            }
+            else
+            {
+                console.log('something broke here')
             }
             
         break;
         case 1:
             if(player === 'rock')
             {
-                console.log('scissor vs rock');
+                console.log('Howd you win? You chose rock vs the computers scissor!');
             }
             else if(player === 'scissor')
             {
-                console.log('scissor vs scissor');
+                console.log('Boring, tied! Two scissors.');
             }
             else if(player === 'paper')
             {
-                console.log('scissor vs paper');
+                console.log('You are SO bad at this.  Computer = scissor. You chose paper.');
+            }
+            else
+            {
+                console.log('something broke here')
             }
         break;
         case 2:
             if (player === 'rock')
             {
-                console.log('paper vs rock');
+                console.log('You are so bad! computer chose paper vs your rock');
             }
             
             else if(player === 'scissor')
             {
-                console.log('paper vs scissor');
+                console.log('Cheater! You chose scissor vs the computers paper!');
             }
             else if (player === 'paper')
             {
-                console.log('paper vs paper');
+                console.log('Tie! try again, paper vs paper.');
+            }
+            else
+            {
+                console.log('something broke here')
             }
         break;
 
     }
          
 }
+
+function game ()
+{
+    
+}
+
 
 getComputerChoice();
 getPlayerChoice();
