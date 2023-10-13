1f14e1a189728ad135252b33157ba5eac0713027
Chris Fedosick <cfedosick@gmail.com>
2023-10-12 21:04:19 -0400

    addition of loop for 5 round game.

--- js/rock/rock.js
+++ js/rock/rock.js
@@ -1,5 +1,8 @@
 let computer
 let player
+let pwin = 0
+let cwin = 0 
+let tie  = 0
 /*for computer - 0 = rock; 1=scissor; 2=paper*/
 
 function getComputerChoice() 
@@ -22,14 +25,17 @@ function compare()
             if(player === 'rock')
             {
                 console.log('Tie! Rock vs. Rock');
+                tie = tie + 1;
             }
             else if (player === 'scissors')
             {
                console.log('Loser! Computer chose rock vs your scissor');
+               cwin = cwin + 1; 
             }
             else if (player === 'paper')
             {
                 console.log('Winner! You chose paper vs the computers rock!');
+                pwin = pwin + 1; 
             }
             else
             {
@@ -41,14 +47,18 @@ function compare()
             if(player === 'rock')
             {
                 console.log('Howd you win? You chose rock vs the computers scissor!');
+                pwin = pwin + 1; 
             }
             else if(player === 'scissor')
             {
                 console.log('Boring, tied! Two scissors.');
+                tie = tie + 1; 
             }
             else if(player === 'paper')
             {
                 console.log('You are SO bad at this.  Computer = scissor. You chose paper.');
+                cwin = cwin + 1; 
+
             }
             else
             {
@@ -59,15 +69,18 @@ function compare()
             if (player === 'rock')
             {
                 console.log('You are so bad! computer chose paper vs your rock');
+                cwin = cwin + 1; 
             }
             
             else if(player === 'scissor')
             {
                 console.log('Cheater! You chose scissor vs the computers paper!');
+                pwin = pwin + 1; 
             }
             else if (player === 'paper')
             {
                 console.log('Tie! try again, paper vs paper.');
+                tie = tie + 1; 
             }
             else
             {
@@ -76,19 +89,41 @@ function compare()
         break;
 
     }
-         
+    return pwin;
+    return cwin;
+    return tie;
+             
 }
 
 function game ()
 {
-    
+    for (let i = 0; i < 5; i++)
+    {
+        getComputerChoice();
+        getPlayerChoice();
+        compare();
+        
+    }
+    if(pwin > cwin)
+    {
+        console.log("Player wins!  Player score: " + pwin + " Computer score: " + cwin + " tie: " + tie); 
+    }
+    else if (cwin > pwin)
+    {
+        console.log("Computer wins! Computer score: " + cwin + " playler score: " + pwin + "tie: " + tie);
+    }
+    else
+    {
+        console.log("Tie!  Ties: " + tie + " Player wins: " + pwin + " computer wins: " + cwin); 
+    }
 }
 
 
 getComputerChoice();
 getPlayerChoice();
-console.log("computer: ", computer);
-console.log("player: ", player);
-compare();
+game();
+console.log("player: " + pwin + " Computer: " + cwin + " Tie: " + tie);
+
+
 
 
